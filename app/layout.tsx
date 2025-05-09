c9334f286663e9f1c3f9310e5af45e03bf857b77
luciano <lazaluciano327@gmail.com>
2025-05-09 09:44:57 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ToastContainer } from "react-toastify"; // ⬅️ ajoute ceci
+import { ToastContainer } from "react-toastify"; // ⬅️ ajoute cecifff
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
